5d201d036e7299c90f069da70caf0483e5529127
Mack2003 <moloyroy8906144923@gmail.com>
2024-09-17 18:09:12 +0530

    saving data to data base

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
 const port = 3000;
+
+const mongoURL = 'mongodb+srv://new-project:v2nGYhJSkb4M0YRv@new-project.jkkdcfq.mongodb.net/test';
+
+// Connect to MongoDB
+mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected successfully'))
+.catch((err) => console.log('MongoDB connection error: ', err));
+
+// Define Schema
+const webhookDataSchema = new mongoose.Schema({
+    type: {
+        type: String,
+        required: true
+    },
+    data: {
+        type: Object,
+        required: true
+    }
+});
+
+// Create Model
+const WebhookData = mongoose.model('webhook_data', webhookDataSchema);
+
 
 // Middleware to parse JSON data from requests
 app.use(express.json());
 
 // Define a POST route
-app.post('/submit', (req, res) => {
+app.post('/submit', async (req, res) => {
   // Accessing the parsed JSON data from the request body
   const requestData = req.body;
-console.log(req.body)
-  // Send a response with the received data
+
+  try {
+    const response = await new WebhookData({
+      type:requestData.type,
+      data: requestData.data
+    })
+  } catch (error) {
+    console.log(error.message)
+  }  // Send a response with the received data
   res.send();
 });
 
